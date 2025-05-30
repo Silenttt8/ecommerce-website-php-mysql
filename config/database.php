aa0c59f2db6dcdedb1aedbf2e638de5f5189fa3a
Silenttt. <dandyrr5@gmail.com>
2025-05-30 16:41:34 +0700

    Add config/database.php 1748598088834

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+// config/database.php
+$host = "localhost";
+$db_name = "ecommerce_db";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    // Set error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
